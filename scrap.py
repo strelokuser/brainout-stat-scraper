a193b865ba45fc4a4512fa4567a9f0d32a53ce42
strelokuser <42389263+strelokuser@users.noreply.github.com>
2024-03-06 02:05:14 +0300

    ver1

--- scrap.py
+++ scrap.py
@@ -9,8 +9,7 @@ from io import BytesIO
 
 def sort_ammo_by_kills(workbook):
     ammo_sheet = workbook['Ammo Stats']
-    
-    # Получаем данные из листа Ammo Stats и сортируем их по убыванию количества убийств (колонка B)
+
     data = []
     for row in ammo_sheet.iter_rows(min_row=2, max_col=2, values_only=True):
         row_data = [cell if hasattr(cell, 'value') else cell for cell in row]
@@ -18,7 +17,6 @@ def sort_ammo_by_kills(workbook):
 
     sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
     
-    # Заменяем существующие данные в листе Ammo Stats отсортированными данными
     for row_number, row_data in enumerate(sorted_data, start=2):
         for col_number, value in enumerate(row_data, start=1):
             ammo_sheet.cell(row=row_number, column=col_number, value=value)
@@ -29,7 +27,6 @@ def add_ammo_types_sheet(workbook, stats_dict):
     ammo_sheet['A1'] = "Caliber"
     ammo_sheet['B1'] = "Kills"
 
-    # Извлекаем данные о пулях и их убийствах
     ammo_info = [key.replace("kills-from-bullet-", "") for key in stats_dict.keys() if "kills-from-bullet-" in key]
 
     for i, ammo in enumerate(ammo_info, start=2):
@@ -43,10 +40,9 @@ def clean_gun_names(workbook):
     for row_number, row in enumerate(guns_sheet.iter_rows(min_row=2, max_col=7, values_only=True), start=2):
         gun_name = row[0]
 
-        # Удаляем "knife-" и "*-"
+        # Удаление "knife-"
         cleaned_gun_name = re.sub(r'knife-|(\w+-)', '', gun_name)
 
-        # Удаляем все "-" внутри строки, если они не находятся в начале или конце
         cleaned_gun_name = cleaned_gun_name.replace('-', '')
         
         guns_sheet[f'A{row_number}'] = cleaned_gun_name
@@ -58,7 +54,7 @@ def clean_ammo_names(workbook):
     for row_number, row in enumerate(ammo_sheet.iter_rows(min_row=2, max_col=2, values_only=True), start=2):
         ammo_caliber = row[0]
 
-        # Удаляем "bullet-"
+        # Удаление "bullet-"
         cleaned_ammo_caliber = ammo_caliber.replace('bullet-', '')
         # cleaned_ammo_caliber = re.sub(r'bullet-|(\w+-)', '', gun_name)
 
@@ -68,7 +64,6 @@ def clean_ammo_names(workbook):
 def sort_guns_by_kills(workbook):
     guns_sheet = workbook['Guns']
     
-    # Получаем данные из листа Guns и сортируем их по убыванию количества убийств (колонка B)
     data = []
     for row in guns_sheet.iter_rows(min_row=2, max_col=7, values_only=True):
         row_data = [cell if hasattr(cell, 'value') else cell for cell in row]
@@ -76,7 +71,6 @@ def sort_guns_by_kills(workbook):
 
     sorted_data = sorted(data, key=lambda x: x[1], reverse=True)
     
-    # Заменяем существующие данные в листе Guns отсортированными данными
     for row_number, row_data in enumerate(sorted_data, start=2):
         for col_number, value in enumerate(row_data, start=1):
             guns_sheet.cell(row=row_number, column=col_number, value=value)
@@ -84,7 +78,6 @@ def sort_guns_by_kills(workbook):
 def add_filters_to_guns(workbook):
     guns_sheet = workbook['Guns']
     
-    # Добавляем заголовки для столбцов Type и Class в фильтры
     guns_sheet.auto_filter.ref = guns_sheet.dimensions
     guns_sheet['F1'] = 'Type'
     guns_sheet['G1'] = 'Class'
@@ -98,7 +91,7 @@ def get_player_stats(player_id):
     response = requests.get(url)
 
     if response.status_code == 200:
-        # Ищем имя игрока в HTML-коде страницы
+        # Поиск имени
         name_pattern = re.compile(r'<p class="text-success"> (.*?) </p><br>', re.DOTALL)
         name_match = name_pattern.search(response.text)
         
@@ -109,7 +102,7 @@ def get_player_stats(player_id):
             print("The player's name could not be found.")
             return None, None
 
-        # Ищем ссылку на картинку
+        # Ссылка на аватарку
         image_pattern = re.compile(r'<img src="(https://avatars.steamstatic.com/.*?)"', re.DOTALL)
         image_match = image_pattern.search(response.text)
         
@@ -146,7 +139,6 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
     general_sheet['A1'] = "Parameter"
     general_sheet['B1'] = "Value"
 
-    # Добавляем "id" и "name" на лист "General"
     general_sheet['E3'] = "id"
     general_sheet['E4'] = "name"
     general_sheet['F3'] = player_id
@@ -167,7 +159,7 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
         print(f"Failed to download the player's image. Status code: {image_response.status_code}")
         return
 
-    # Информация для General
+    # Заполнение General
     general_info = [
         "time-spent", "level", "score", "rating", "kpd", "kills", "deaths",
         "headshots", "longshots", "head-to-head-kills", "double-kills", "triple-kills",
@@ -177,7 +169,7 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
         general_sheet[f'A{i}'] = parameter
         general_sheet[f'B{i}'] = stats_dict.get(parameter, 0)
 
-    # Дополнительные листы
+    # Листы
     sheets_data = {
         "Resources": ["ru", "gears", "skillpts", "nuclear-material"],
         "Gamemodes": [
@@ -213,7 +205,7 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
             sheet[f'A{i}'] = parameter
             sheet[f'B{i}'] = stats_dict.get(parameter, 0)
 
-    # Guns лист
+    # Лист Guns
     guns_sheet = workbook.create_sheet(title="Guns")
     guns_sheet['A1'] = "Weapon"
     guns_sheet['B1'] = "Kills"
@@ -233,14 +225,12 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
 
     add_ammo_types_sheet(workbook, stats_dict)
     
-    # Списки оружия для разных типов
+    # Оружие по типу
     primary_weapons = ["galil", "mdrc", "lr300", "auga3", "sgi5k", "aswal", "groza", "aek", "gilboa-snake", "an94", "m16", "car15", "a762", "m4a1", "ls5", "rpk16", "tar21", "ft200m", "sr3", "ak105", "sig552", "aug", "hk417", "scar", "hk416", "ak12", "ak105bullpup", "ak74", "aksu", "fnfal", "akm", "ares-shrike", "scarl", "ak15", "ak74m", "famas", "aac", "ots141a", "vhs2", "acr", "m16a3", "m4a1-desert", "fn2000", "9a91", "akm74-2", "m4a1-zoom", "stg", "ar15", "ar57", "sr47", "ar18", "imbel",  "qbz", "g36c", "ak108", "cima", "ak9", "pdrc", "p91"]
     
     smg_weapons = ["vityaz", "ump", "mp5", "kiparis", "bizon", "mp5sd6", "p90", "mac10", "thompson", "augpara", "mp9", "agram", "scorpion-evo", "uzi", "pp27", "pp2000", "mp28", "m3gg", "mp40", "kriss", "mp7a2", "mpx", "mp7", "veresk"]
     
     shotgun_weapons = ["six12", "mossberg590", "aa12", "spas12", "toz34", "saiga", "spr220", "protecta", "stoeger", "mts255", "1887", "sauer", "mts569", "qbu88", "nova", "keltec", "ithaca", "qbs", "mp94", "m1014", "m1897"]
-    
-    # pp_weapons = ["m60", "hunter-bow", "rpk16", "rg6", "crossbow", "mg42", "pkm", "fnminimi", "m79", "mg36", "ppsh", "tul1", "pecheneg", "aek999", "rpg7", "gm94", "paint", "katana", "water", "l86", "roks3", "rpd", "rpk74", "chainsaw", "mg34", "serf", "piolet", "snowball-blaster"]
     
     sniper_weapons = ["dvl10", "kac", "vssk", "vss", "sniper", "g3", "wa2000", "sv98", "mosin", "m98b", "svu", "m98k", "garand", "vssm", "vsk94", "svds", "rsaas", "scout", "zastava", "dtsrs", "hecate", "lisle", "sks", "fg42", "gewehr43", "m1", "m14", "m21", "m82", "svd", "m110", "hksl8", "rem7600", "m95"]
 
@@ -268,7 +258,6 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
         guns_sheet[f'D{i}'] = stats_dict.get(f"headshots-from-weapon-{gun}", 0)
         guns_sheet[f'E{i}'] = stats_dict.get(f"durability-of-weapon-{gun}", 0)
 
-        # Определение типа оружия
         weapon_type = "primary"
         weapon_class = "-"
 
@@ -277,7 +266,6 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
         elif gun in knife_weapons:
             weapon_type = "knife"
         elif gun in instrument_weapons:
-            # Для оружия типа "instrument" используем правильные ключи
             weapon_type = "instrument"
             weapon_class = "-instrument"
 
@@ -314,11 +302,7 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
         guns_sheet[f'F{i}'] = weapon_type
         guns_sheet[f'G{i}'] = weapon_class
 
-
-    # Сортировка листа Guns по количеству убийств
-    # guns_sheet.sort(column='B', descending=True)
-
-    # Устанавливаем ширину колонок
+    # Ширина колонок
     for sheet in workbook.sheetnames:
         for column in workbook[sheet].columns:
             max_length = 0
@@ -331,6 +315,7 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
                     pass
             adjusted_width = max(10, (max_length + 2))  # Set a minimum width of 10 for empty columns
             workbook[sheet].column_dimensions[column[0].column_letter].width = adjusted_width
+
     # Санитизация имени файла
     sanitized_player_name = re.sub(r'[\/:*?"<>|]', '_', player_name)
 
@@ -344,12 +329,12 @@ def create_excel_table(stats_dict, player_name, player_image_url, excel_filename
 
 
 
-    # Сохраняем таблицу
+    # Сейв таблицы
     excel_filename = f"{sanitized_player_name}.xlsx"
     workbook.save(excel_filename)
     print(f"Player statistics are saved in a file {excel_filename}")
 
-# Запрашиваем ID игрока
+# Запрос ID 
 # player_id = 631254
 player_id = input("Player's ID: ")
 player_name, player_stats_str, player_image_url = get_player_stats(player_id)
